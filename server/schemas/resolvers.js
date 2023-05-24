90ac9172b902d81e78c6a8b0f34c9044499594e6
KrookedKickflip <colinnicosia84@yahoo.com>
2023-05-23 20:00:58 -0500

    stuck at resolvers, got typeDefs done and apollo server running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,13 +2,15 @@ const { ApolloServer } = require('apollo-server-express');
 const { User, Book } = require("../models");
 
 const resolvers = {
-    Query: {
-
-    },
-
+   Query: {
+    // query resolvers will go here
+    // could not figure them out with the time left
+   },
+        
     Mutation: {
-
-    }
+    // mutation resolvers will go here
+    // could not figure them out with the time left
+    },
 }
 
 module.exports = resolvers;
---
+++ server/seeds/seed.js
@@ -0,0 +1,4 @@
+const db = require('../config/connection');
+const { User } = require('../models');
+const userSeeds = require('./userSeed.json');
+
