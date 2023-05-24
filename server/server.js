1b41c19ac8144c88e372c13cd63a071b8b2239f7
KrookedKickflip <colinnicosia84@yahoo.com>
2023-05-23 21:10:13 -0500

    ugh

--- server/server.js
+++ server/server.js
@@ -16,10 +16,10 @@ const server = new ApolloServer ({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+//if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 //app.use(routes);
 
